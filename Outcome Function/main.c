7536bebda64b281acb4512378058df795eee2710
AaronD7492 <adagloria7492@conestogac.on.ca>
2023-11-02 21:31:45 -0400

    Add project files.

---
+++ Outcome Function/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+//Req_001: the system shall be capable of 
+
+int main() {
+
+	printf("Hello World\n");
+
+	return 1;
+}
+
+int Add(int x, int y) {
+	int sum = 0;
+	sum = x - y;
+	return sum;
+}
