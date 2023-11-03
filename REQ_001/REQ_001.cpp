bfb4e72722a6c2598c4163841b9defcb6418e671
AaronD7492 <adagloria7492@conestogac.on.ca>
2023-11-03 09:59:18 -0400

    Thrid failing test added for OutcomeDecider(). Previous two tests have passed.

--- REQ_001/REQ_001.cpp
+++ REQ_001/REQ_001.cpp
@@ -22,8 +22,16 @@ namespace REQ001
 		}
 		TEST_METHOD(OutcomeDeciderReturnValuePlayer1)
 		{
-			char* Result = OutcomeDecider("Paper", "Paper");
+			char* Result = OutcomeDecider("Paper", "Rock");
 			char* Expected = "Player1";
+			int outcome = strcmp(Result, Expected);
+			int actual = 0;
+			Assert::AreEqual(actual, outcome);
+		}
+		TEST_METHOD(OutcomeDeciderReturnValuePlayer2)
+		{
+			char* Result = OutcomeDecider("Rock", "Scissors");
+			char* Expected = "Player2";
 			int outcome = strcmp(Result, Expected);
 			int actual = 0;
 			Assert::AreEqual(actual, outcome);
